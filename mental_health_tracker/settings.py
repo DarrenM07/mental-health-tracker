1fe9f980a1b71982c3bfe0a925e3225a1344d491
DarrenM07 <darrenms7120@gmail.com>
2024-08-28 13:17:33 +0700

    changed settings.py host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(@a@%#hsr!uqx^5%tn=$%xad1r6u8m)2m_fru4m(0o4_+yu-c-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","darren-marcello-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
